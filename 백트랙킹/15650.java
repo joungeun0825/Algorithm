fe72c54f6729248995d40948d66855d24e841926
joungeun0825 <160833920+joungeun0825@users.noreply.github.com>
2024-03-29 18:06:39 +0900

    N과 M (2)

---
+++ 백트랙킹/15650.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    static int[] arr;
+    static boolean[] v;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        arr = new int[M];
+        v = new boolean[N];
+        dfs(N,M,0,-1);
+    }
+    
+    public static void dfs(int N, int M, int depth, int pre){
+
+        if(depth==M){
+            for (int a : arr) {
+                System.out.print(a+" ");    
+            }
+            System.out.println();
+            return;
+        }
+        
+        for (int i=0;i<N;i++) {
+            
+            if(!v[i] && i>pre){
+                v[i] = true;
+                arr[depth]=i+1;
+                dfs(N,M,depth+1,i);
+                v[i] = false;
+            }
+            
+        }
+        
+        
+    }
+    
+}
