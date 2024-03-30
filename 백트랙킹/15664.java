92028c697ed65b35b38b9c52338cecaffba47272
joungeun0825 <160833920+joungeun0825@users.noreply.github.com>
2024-03-30 16:23:18 +0900

    N과 M (10)

---
+++ 백트랙킹/15664.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    static int[] arr;
+    static int[] dic;
+    static boolean[] v;
+    static int before;
+    public static StringBuilder sb = new StringBuilder();
+    public static LinkedHashSet<String> set = new LinkedHashSet<>();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine()," ");
+        dic = new int[N];
+        for(int i=0;i<N;i++){
+            dic[i] = Integer.parseInt(st.nextToken());
+        }
+        arr = new int[M];
+        Arrays.sort(dic);
+        v = new boolean[N];
+        before = -1; 
+        dfs(N,M,0);
+        
+        for (String s : set) {
+            System.out.println(s);
+        }
+        
+    }
+
+    public static void dfs(int N, int M, int depth){
+
+        if (depth == M) {
+            StringBuilder sb = new StringBuilder();
+            for (int num : arr) {
+                sb.append(num).append(" ");
+            }
+            set.add(sb.toString().trim()); // 배열의 값을 문자열로 변환하여 HashSet에 추가
+            return;
+        }
+        
+        for(int i=0;i<N;i++){
+            if(!v[i]&&dic[i]>=before){
+                v[i]= true;
+                arr[depth] = dic[i];
+                before = dic[i];
+                dfs(N,M,depth+1);
+                before = -1;
+                v[i]=false;
+            }             
+        }
+    }
+}
