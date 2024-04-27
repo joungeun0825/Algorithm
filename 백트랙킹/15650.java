bb4b6b5009666bebedf37a23262a620ee54881d9
joungeun0825 <160833920+joungeun0825@users.noreply.github.com>
2024-04-28 00:09:10 +0900

    Update 15650.java

--- 백트랙킹/15650.java
+++ 백트랙킹/15650.java
@@ -1,43 +1,50 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
-// The main method must be in a class named "Main".
-class Main {
-    static int[] arr;
-    static boolean[] v;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+public class Main {
+	static ArrayList<Integer> list;
 
-        arr = new int[M];
-        v = new boolean[N];
-        dfs(N,M,0,-1);
-    }
-    
-    public static void dfs(int N, int M, int depth, int pre){
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		list = new ArrayList<>();
 
-        if(depth==M){
-            for (int a : arr) {
-                System.out.print(a+" ");    
-            }
-            System.out.println();
-            return;
-        }
-        
-        for (int i=0;i<N;i++) {
-            
-            if(!v[i] && i>pre){
-                v[i] = true;
-                arr[depth]=i+1;
-                dfs(N,M,depth+1,i);
-                v[i] = false;
-            }
-            
-        }
-        
-        
-    }
-    
+		// 9876543210 -> max
+
+		if (N <= 10) {
+			System.out.println(N);
+		} else if (N > 1022) {
+			System.out.println(-1);
+		} else {
+			for (int i = 0; i < 10; i++) {
+				Cal(i, 1);
+			}
+			// 순서대로 저장이 안되어있기 때문에 sort가 필요함.
+			Collections.sort(list);
+			System.out.println(list.get(N));
+		}
+
+	}
+	
+	private static void Cal(int num, int depth) {
+		//9876543210 이 마지막임. 총 10자리.
+		if (depth > 10) {
+			return;
+		}
+		list.add(num);
+		
+		// 처음에 num이 0이면 pass 
+		// num이 1이면 i가 0일때만. => 1뒤에 올수있는 감소하는 수들... 1 , 10
+		// num이 2면 2,20, 21, 210 
+		// num이 3이면 3, 30, 31,310,32,320,321,322
+		// ..이런식으로 9까지
+		for (int i = 0; i < 10; i++) {
+			if (num % 10 > i) {
+				Cal((num * 10) + i, depth + 1);
+			}
+		}
+		return;
+	}
+
 }
