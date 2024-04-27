86d118ab763d899fb764bab962a035b5f8384e50
joungeun0825 <160833920+joungeun0825@users.noreply.github.com>
2024-04-28 00:09:39 +0900

    Update 1038.java

--- 백트랙킹/1038.java
+++ 백트랙킹/1038.java
@@ -1,44 +1,50 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	static ArrayList<Integer> list;
 
-    static int n;
-    static List<Long> list = new ArrayList<>();
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		list = new ArrayList<>();
 
-    public static void comb(long number) {
-        list.add(number);
-        long val = number % 10;
-        if (val == 0) {
-            return;
-        }
+		// 9876543210 -> max
 
-        for (long i = val - 1; i >= 0; i--) {
-            long temp = number * 10 + i;
-            comb(temp);
-        }
-    }
+		if (N <= 10) {
+			System.out.println(N);
+		} else if (N > 1022) {
+			System.out.println(-1);
+		} else {
+			for (int i = 0; i < 10; i++) {
+				Cal(i, 1);
+			}
+			// 순서대로 저장이 안되어있기 때문에 sort가 필요함.
+			Collections.sort(list);
+			System.out.println(list.get(N));
+		}
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	}
+	
+	private static void Cal(int num, int depth) {
+		//9876543210 이 마지막임. 총 10자리.
+		if (depth > 10) {
+			return;
+		}
+		list.add(num);
+		
+		// 처음에 num이 0이면 pass 
+		// num이 1이면 i가 0일때만. => 1뒤에 올수있는 감소하는 수들... 1 , 10
+		// num이 2면 2,20, 21, 210 
+		// num이 3이면 3, 30, 31,310,32,320,321,322
+		// ..이런식으로 9까지
+		for (int i = 0; i < 10; i++) {
+			if (num % 10 > i) {
+				Cal((num * 10) + i, depth + 1);
+			}
+		}
+		return;
+	}
 
-        n = sc.nextInt();
-
-        if (n <= 10) {
-            System.out.println(n);
-            return;
-        } else if (n >= 1023) {
-            System.out.println(-1);
-            return;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            comb(i);
-        }
-
-        Collections.sort(list);
-        System.out.println(list.get(n));
-    }
 }
