91971c34149c692368a29d450b44271df219d1de
JoungEun Park <160833920+joungeun0825@users.noreply.github.com>
2025-01-05 23:43:01 +0900

    풍선 터뜨리기(ArrayDeque)

--- 자료구조/2346.java
+++ 자료구조/2346.java
@@ -1,47 +1,41 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-// The main method must be in a class named "Main".
-class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
+public class Main {
 
-        int N = Integer.parseInt(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] val = new int[n][2];
 
-        //deque 초기화
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        Deque<int[]> deq = new ArrayDeque<>();
-        for(int i=1;i<N+1;i++){
-            deq.offerLast(new int[] { i, Integer.parseInt(st.nextToken()) });
+        Deque<int[]> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            val[i][0] = i+1;
+            val[i][1] = sc.nextInt();
+            queue.add(val[i]);
         }
 
-        //첫 풍선 터트려서 종이 안에 수 확인
-         
-        int[] pop = {0,0};
-        pop = deq.pollFirst();
-        int val = pop[1];
-        sb.append(pop[0]).append(' ');
-        while(!deq.isEmpty()){
-            if(val>0){
-                for(int x=1;x<val;x++){
-                    deq.offerLast(deq.pollFirst());
+        List<Integer> result = new ArrayList<>();
+        int[] now = queue.poll();
+        result.add(now[0]);
+
+        while (!queue.isEmpty()) {
+            if(now[1] > 0) {
+                for(int i =0;i<now[1]-1;i++){
+                    queue.add(queue.poll());
                 }
-                pop = deq.pollFirst();
-                val = pop[1];
-                sb.append(pop[0]).append(' ');
-            }
-            else{
-                for(int x=1;x<Math.abs(val);x++){
-                    deq.offerFirst(deq.pollLast());
+                now = queue.poll();
+                result.add(now[0]);
+            }else{
+                for(int i =0;i<Math.abs(now[1])-1;i++){
+                    queue.addFirst(queue.pollLast());
                 }
-                pop = deq.pollLast();
-                val = pop[1];
-                sb.append(pop[0]).append(' ');
+                now = queue.pollLast();
+                result.add(now[0]);
             }
         }
-    
-        System.out.println(sb);
+
+        for(int r : result) {
+            System.out.print(r+" ");
+        }
     }
 }
