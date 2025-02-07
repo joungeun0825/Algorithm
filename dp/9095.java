70d1a6ab1d35a339c4cce15696f0dbe2ed1e8178
JoungEun Park <160833920+joungeun0825@users.noreply.github.com>
2025-02-07 16:41:40 +0900

    1, 2, 3 더하기

--- dp/9095.java
+++ dp/9095.java
@@ -1,32 +1,25 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-// The main method must be in a class named "Main".
-class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int[] dp = new int[11];
-        dp[1]=1;
-        dp[2]=2;
-        dp[3]=4;
-        int[] arr = new int[T];
-        
-        for (int i=0; i<T; i++) {
-            int N = sc.nextInt();
-            
-            for (int j=4; j<=N; j++) {
-                 dp[j] = dp[j-1] + dp[j-2] + dp[j-3];   
-            }
-            arr[i] = dp[N];
+        int T = sc.nextInt(); // 테스트 케이스 개수
+        int[] dp = new int[11]; // n은 최대 10까지
+
+        // 초기 조건 설정
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        // DP 테이블 채우기
+        for (int i = 4; i <= 10; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-        
-        for (int i=0; i<T; i++) {
-            System.out.println(arr[i]);
-            
+
+        // 테스트 케이스 실행
+        for (int t = 0; t < T; t++) {
+            int n = sc.nextInt();
+            System.out.println(dp[n]);
         }
-        
     }
-    
 }
